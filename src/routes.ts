1a8e24b55fa97cd61cb123030871aa0d0a5dab25
Kayz-mann <kayodefredrickbalogun@gmail.com>
2022-05-08 17:24:42 +0100

    crud operation with user validation for products api

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Request, Response } from 'express';
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from './controller/product.controller';
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from './controller/session.controller';
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validate from './middleware/validateResource';
+import { createProductSchema, updateProductSchema, getProductSchema, deleteProductSchema } from './schema/product.schema';
 import { createSessionSchema } from './schema/session.schema';
 import { createUserSchema } from './schema/user.schema';
 
@@ -17,6 +19,24 @@ function routes(app: Express) {
     app.get('/api/sessions', requireUser, getUserSessionsHandler);
 
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+//products
+    app.post("/api/products", [requireUser, validate(createProductSchema)],
+        createProductHandler
+    );
+
+    app.put("/api/products/:productId", [requireUser, validate(updateProductSchema)],
+    updateProductHandler
+    );
+    
+    app.get("/api/products/:productId", [ validate(getProductSchema)],
+    getProductHandler
+    );
+    
+    app.delete("/api/products/:productId", [requireUser,validate(deleteProductSchema)],
+    deleteProductHandler
+);
+
 }
 
 export default routes;
